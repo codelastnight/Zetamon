fae86dd4659c05d2c91100ab6ddf201380d714c1
not simon <fujizhang2525@gmail.com>
2018-09-30 00:51:14 -0700

    mobile support header

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
 //AOS activation script - animate on scroll
-AOS.init({
- duration: 500
-});
 
-function menuToggle(button){
+
+function menuToggle(button) {
   $(button).toggleClass("is-active");
-  $(".header-overlay").toggleClass("header-overlay-toggle")
+//  $('body').toggleClass("no-scroll");
+
+  var $listSort = $('.header-overlay');
+  if ($listSort.attr('aria-hidden')) {
+    $listSort.removeAttr('aria-hidden');
+  } else {
+    $listSort.attr('aria-hidden', true);
+  }
 }
+
+$(".header-overlay").click(function(e){
+  if (e.target !== this)
+    return;
+
+  $(this).attr('aria-hidden', true);
+  $('.hamburger').removeClass("is-active");
+  $('body').removeClass("no-scroll");
+});
+// shows menu on desktops
+function menuDesktop(x) {
+  if (x.matches) { // if desktop
+    $('.header-overlay').removeAttr('aria-hidden');
+    $('.hamburger').removeClass("is-active");
+    $('body').removeClass("no-scroll");
+  } else { //if mobile
+    $('.header-overlay').attr('aria-hidden', true);
+
+  }
+}
+var x = window.matchMedia("(min-width: 1280px)")
+menuDesktop(x)
+x.addListener(menuDesktop)
+
+$(document).ready(function() {
+  AOS.init({
+
+    duration: 500
+  });
+});
