2b918a0879376a82e27eb036cf4af98d8be45be8
loopiellcdev <loopiellcdev@gmail.com>
2018-10-06 19:06:51 -0700

    COUNTRY ROADS

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // swiper is the image gallary thing for the landing page
   //initialize swiper when document ready
 
-  var mySwiper = new Swiper ('.swiper-container', {
+
+  var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -15,10 +16,32 @@ $(document).ready(function () {
       clickable: true
     },
     autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-      },
+      delay: 4000,
+      disableOnInteraction: false,
+    },
 
-  })
- //$('main').overlayScrollbars({ });
+  });
+
+  $('#calendar').fullCalendar({
+    // put your options and callbacks here
+    defaultView: 'listMonth',
+    googleCalendarApiKey: 'AIzaSyBJJ13IeXcBSsy0-E1ls6YdCnrZkG6tmtI',
+    eventSources: [
+      {
+        googleCalendarId: 'saintsrobotics.com_2ha1cp70qh2dcb61rlkq1tlaas@group.calendar.google.com'
+      },
+      {
+        googleCalendarId: 'saintsrobotics.com_2ha1cp70qh2dcb61rlkq1tlaas@group.calendar.google.com'
+
+      },
+      {
+        googleCalendarId: 'saintsrobotics.com_31th07n6achakl2jof6th85dfs@group.calendar.google.com'
+
+      }
+
+    ],
+
+
+  });
+  //$('main').overlayScrollbars({ });
 });
