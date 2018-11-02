817fad5a3835102d898bdb80b0179098fc259ff9
not simon <fujizhang2525@gmail.com>
2018-11-01 21:33:52 -0700

    disable buggy js

--- js/homepage.js
+++ js/homepage.js
@@ -23,28 +23,28 @@ $(document).ready(function() {
 
   //$('main').overlayScrollbars({ });
 });
-formatGoogleCalendar.init({
-  calendarUrl: [
-    //'https://www.googleapis.com/calendar/v3/calendars/saintsrobotics.com_31th07n6achakl2jof6th85dfs@group.calendar.google.com/events?key=AIzaSyBJJ13IeXcBSsy0-E1ls6YdCnrZkG6tmtI', 'https://www.googleapis.com/calendar/v3/calendars/saintsrobotics.com_2ha1cp70qh2dcb61rlkq1tlaas@group.calendar.google.com/events?key=AIzaSyBJJ13IeXcBSsy0-E1ls6YdCnrZkG6tmtI'
-  ],
-  past: false,
-  upcoming: true,
-  sameDayTimes: true,
-  dayNames: true,
-  pastTopN: -1,
-  upcomingTopN: 10,
-  itemsTagName: 'li',
-  upcomingSelector: '#events-upcoming',
-  recurringEvents: true,
-  upcomingHeading: '<h2>Upcoming events</h2>',
-
-  format: [
-    '*date*',
-    ': ',
-    '*summary*',
-    ' — ',
-    '*description*',
-    ' in ',
-    '*location*'
-  ]
+// formatGoogleCalendar.init({
+//   calendarUrl: [
+//     //'https://www.googleapis.com/calendar/v3/calendars/saintsrobotics.com_31th07n6achakl2jof6th85dfs@group.calendar.google.com/events?key=AIzaSyBJJ13IeXcBSsy0-E1ls6YdCnrZkG6tmtI', 'https://www.googleapis.com/calendar/v3/calendars/saintsrobotics.com_2ha1cp70qh2dcb61rlkq1tlaas@group.calendar.google.com/events?key=AIzaSyBJJ13IeXcBSsy0-E1ls6YdCnrZkG6tmtI'
+//   ],
+//   past: false,
+//   upcoming: true,
+//   sameDayTimes: true,
+//   dayNames: true,
+//   pastTopN: -1,
+//   upcomingTopN: 10,
+//   itemsTagName: 'li',
+//   upcomingSelector: '#events-upcoming',
+//   recurringEvents: true,
+//   upcomingHeading: '<h2>Upcoming events</h2>',
+//
+//   format: [
+//     '*date*',
+//     ': ',
+//     '*summary*',
+//     ' — ',
+//     '*description*',
+//     ' in ',
+//     '*location*'
+//   ]
 });
