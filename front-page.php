434195a5d2ccbc59f9efd8b6a451a8c9f1053757
not simon <fujizhang2525@gmail.com>
2018-11-19 08:35:26 -0800

    front page

--- front-page.php
+++ front-page.php
@@ -8,18 +8,24 @@
  *
  * @package saintsrobotics
  */
-function frontPageContent($pagename) {
+function frontPageContent($pagename,$alignment ='') {
   $your_query = new WP_Query( 'pagename='.$pagename );
   // "loop" through query (even though it's just one page)
   while ( $your_query->have_posts() ) : $your_query->the_post();
+      ?><div class='<?=$alignment?>'>
+        <?php
       the_title('<h1 class="text-huge" id='.$pagename.'>', '</h1>' );
       the_content();
+      ?>
+      <button class="button button-secondary" href="#">Learn more</button>
+
+    </div><?php
   endwhile;
   // reset post data (important!)
   wp_reset_postdata();
 }
 get_header(); ?>
-<div id="home"class="landing">
+<div id="home" class="landing">
   <div class="image-slider">
   <?php
   $shortcode = get_theme_mod('landing_slider_shortcode', 'none');
@@ -33,7 +39,7 @@ get_header(); ?>
   if ($messageColor == "none") {
      $messageHidden = "aria-hidden='true'";
   };
-?>
+  ?>
   <div class="absolute-top " <?= $messageHidden ?>>
     <div class="container">
       <div class="section">
@@ -109,10 +115,21 @@ get_header(); ?>
   <section class="section" id="trigger-menu">
 
   <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-5 ">
+
     <?php
-  frontPageContent("about-us")
-      ?>
+        frontPageContent("about-us","align-right")
+    ?>
+
+      </div>
+      <div class="col-md-7">
+
+          <div class="div-round gmap hidden-xs hidden-sm">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d672.2916398599913!2d-122.11685113967444!3d47.62289350181041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906dbdfa0f55b7%3A0x35a096d8775d8148!2sNorthup+Way+%26+168th+Ave+NE!5e0!3m2!1sen!2sus!4v1542588102080" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+          </div>
+
+
       </div>
     </div>
   </section>
@@ -157,7 +174,7 @@ get_header(); ?>
 </div>
 
 
-</div>
+
 <script>
 jQuery(document).ready(function($) {
   $('.menu-item-home > a').attr("href", "#trigger-menu");
