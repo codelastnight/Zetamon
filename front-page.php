76b576323e176391f895ec8ec4e44bfd4363e59a
not simon <fujizhang2525@gmail.com>
2018-11-19 10:16:13 -0800

    front page

--- front-page.php
+++ front-page.php
@@ -14,7 +14,23 @@ function frontPageContent($pagename,$alignment ='') {
   while ( $your_query->have_posts() ) : $your_query->the_post();
       ?><div class='<?=$alignment?>'>
         <?php
-      the_title('<h1 class="text-huge" id='.$pagename.'>', '</h1>' );
+      the_title('<h1 class="text-huge " id='.$pagename.'>', '</h1>' );
+      the_content();
+      ?>
+      <button class="button button-secondary" href="#">Learn more</button>
+
+    </div><?php
+  endwhile;
+  // reset post data (important!)
+  wp_reset_postdata();
+}
+function frontPageContentLight($pagename,$alignment ='') {
+  $your_query = new WP_Query( 'pagename='.$pagename );
+  // "loop" through query (even though it's just one page)
+  while ( $your_query->have_posts() ) : $your_query->the_post();
+      ?><div class='<?=$alignment?>'>
+        <?php
+      the_title('<h1 class="text-huge text-with-subtitle" id='.$pagename.'>', '</h1>' );
       the_content();
       ?>
       <button class="button button-secondary" href="#">Learn more</button>
@@ -49,14 +65,10 @@ get_header(); ?>
     </div>
   </div>
   <div class="container center-vertical">
-
-
     <div class="section ">
-      <div class="">
       <div class="row ">
         <div class="col-md-6 ">
           <div class="landing-logo">
-
             <p>INTERLAKE</p>
 
             <p>SAINTS</p>
@@ -69,7 +81,6 @@ get_header(); ?>
             </ul>
       </div>
       </div>
-    </div>
       <div class="row">
         <div class="col-md-12">
           <div class="landing-logo">
@@ -110,69 +121,73 @@ get_header(); ?>
   </div>
 </div>
 
+<div class="about-page">
+  <div class='container'>
 
-<div class='container'>
-  <section class="section" id="trigger-menu">
+    <section class="section" id="trigger-menu" >
+      <div class="section">
 
-  <div class="row">
-    <div class="col-md-5 ">
 
-    <?php
-        frontPageContent("about-us","align-right")
-    ?>
+      <div class="row is-flex">
+        <div class="col-md-5 ">
 
+        <?php
+            frontPageContent("about-us","align-right")
+        ?>
+
+        </div>
+        <div class="col-md-7">
+
+            <div class="div-round gmap ">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d672.2916398599913!2d-122.11685113967444!3d47.62289350181041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906dbdfa0f55b7%3A0x35a096d8775d8148!2sNorthup+Way+%26+168th+Ave+NE!5e0!3m2!1sen!2sus!4v1542588102080" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+            </div>
+
+        </div>
       </div>
-      <div class="col-md-7">
-
-          <div class="div-round gmap hidden-xs hidden-sm">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d672.2916398599913!2d-122.11685113967444!3d47.62289350181041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906dbdfa0f55b7%3A0x35a096d8775d8148!2sNorthup+Way+%26+168th+Ave+NE!5e0!3m2!1sen!2sus!4v1542588102080" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
-
-          </div>
-
-
       </div>
-    </div>
-  </section>
-</div>
-<div class="section hero-light" style="">
-    <div class="container text-center">
-        <h3 class="text-huge text-with-subtitle">We can be heroes</h3>
-        <h4 class="text-big">just for one day</h4>
-    </div>
-</div>
-<div class='container '>
-  <section class="section ">
-
-  <div class="row">
-    <div class="col-md-12">
-    <?php
-    frontPageContent("community-outreach") ?>
+    </section>
+  </div>
+  <div class="section hero-light" style="">
+      <div class="container">
+        <?php  frontPageContentLight("community-outreach","align-center") ?>
       </div>
-    </div>
-  </section>
-</div>
-<div class='container'>
-  <section class="section">
-  <div class="row">
-    <div class="col-md-12">
+  </div>
+  <div class='container '>
+    <section class="section ">
+
+    <div class="row">
+      <div class="col-md-12">
       <?php
-      frontPageContent("organization") ?>
+      frontPageContent("community-outreach") ?>
+        </div>
       </div>
-    </div>
-  </section>
-</div>
-<div class='container'>
-  <section class="section">
-
-  <div class="row">
-    <div class="col-md-12">
-      <?php
-      frontPageContent("sponsors") ?>
+    </section>
+  </div>
+  <div class='container'>
+    <section class="section">
+    <div class="row">
+      <div class="col-md-12">
+        <?php
+        frontPageContent("organization") ?>
+        </div>
       </div>
-    </div>
-  </section>
-</div>
+    </section>
+  </div>
+  <div class='container'>
+    <section class="section">
 
+    <div class="row">
+      <div class="col-md-12">
+        <?php
+        frontPageContent("sponsors") ?>
+        </div>
+      </div>
+    </section>
+  </div>
+
+
+</div>
 
 
 <script>
